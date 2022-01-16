1005c1163591d4c88d61b697abed4e2b4cb13f01
Chandan Kumar <cksharma122@gmail.com>
2022-01-16 13:13:45 +0530

    Intial commit - setting up project

---
+++ src/main/kotlin/com/infydex/virtual_trading/VirtualTradingApplication.kt
@@ -0,0 +1,11 @@
+package com.infydex.virtual_trading
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VirtualTradingApplication
+
+fun main(args: Array<String>) {
+	runApplication<VirtualTradingApplication>(*args)
+}
