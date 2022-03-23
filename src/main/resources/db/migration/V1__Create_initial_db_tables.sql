192c5b83766525d8ecc1c8ad248cbdbfd709c212
Chandan Kumar <cksharma122@gmail.com>
2022-03-23 16:53:11 +0530

    Change migration to create watch list with minor type corrections

--- src/main/resources/db/migration/V1__Create_initial_db_tables.sql
+++ src/main/resources/db/migration/V1__Create_initial_db_tables.sql
@@ -3,17 +3,19 @@ CREATE TABLE IF NOT EXISTS investor
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(200),
     email      VARCHAR(100),
-    phone      VARCHAR(100),
+    phone      VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT now()
 );
 
+CREATE TYPE TRANSACTION_TYPE AS ENUM ('credit', 'withdraw');
+
 CREATE TABLE IF NOT EXISTS fund
 (
-    id          SERIAL PRIMARY KEY,
-    type        VARCHAR(64),
-    investor_id SERIAL,
-    amount      decimal,
-    created_at  TIMESTAMP DEFAULT now(),
+    id               SERIAL PRIMARY KEY,
+    transaction_type TRANSACTION_TYPE,
+    investor_id      SERIAL,
+    amount           DECIMAL,
+    created_at       TIMESTAMP DEFAULT now(),
     CONSTRAINT fk_investor_id
         FOREIGN KEY (investor_id)
             REFERENCES investor (id)
@@ -26,10 +28,20 @@ CREATE TABLE IF NOT EXISTS investment
     investor_id  SERIAL,
     stock_symbol VARCHAR(200),
     status       VARCHAR(200),
-    price        decimal,
+    price        DECIMAL,
     created_at   TIMESTAMP DEFAULT now(),
     CONSTRAINT fk_investor_id
         FOREIGN KEY (investor_id)
             REFERENCES investor (id)
 );
 
+CREATE TABLE IF NOT EXISTS watchlist
+(
+    id          SERIAL PRIMARY KEY,
+    investor_id SERIAL,
+    created_at  TIMESTAMP DEFAULT now(),
+    stocks      TEXT[],
+    CONSTRAINT fk_investor_id
+        FOREIGN KEY (investor_id)
+            REFERENCES investor (id)
+);
