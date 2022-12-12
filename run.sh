770d5b86c69dde373536cb9d7a5e9d31d527c476
Chandan Kumar <cksharma122@gmail.com>
2022-12-12 22:04:08 +0530

    Update serive run statement

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-__apply_git_hooks() {
+__git_hooks() {
   cp scripts/hooks/pre-commit .git/hooks/
 }
 
-__standalone_tmux() {
+__service() {
   docker-compose build
   type tmuxinator > /dev/null 2>&1 || brew install tmuxinator
   touch ~/.tmux.conf
   tmuxinator local || tmuxinator start -p .tmuxinator_default.yml
 }
 
-__apply_git_hooks
-__standalone_tmux
+__git_hooks
+__service
